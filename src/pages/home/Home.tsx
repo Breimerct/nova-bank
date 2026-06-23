f7e98c541b88b346b2bca10a36b06b48d55f481b
breimerct <breimerct@gmail.com>
2026-06-23 18:24:36 -0500

    add SidePanel for creating transfers with integrated CreateTransferForm

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,10 @@
 import BalanceCard from "@/components/balance-card/BalanceCard.tsx"
 import Button from "@/components/ui/button/button.tsx"
-import { PlusCircle } from "lucide-react"
+import { PlusCircle, SavePlus } from "lucide-react"
 import TransferList from "@/components/transfer-list/TransferList.tsx"
 import TransferFilter from "@/components/trasnfer-filter/TransferFilter.tsx"
+import SidePanel from "@/components/side-panel/SidePanel.tsx"
+import CreateTransferForm from "@/components/create-transfer/CreateTransferForm.tsx"
 
 function Home() {
   return (
@@ -14,13 +16,26 @@ function Home() {
       />
 
       <article className="mt-8">
-        <div className="flex items-center justify-between mb-4">
-          <h4 className="font-semibold text-lg">Transfers</h4>
+        <div className="mb-4 flex items-center justify-between">
+          <h4 className="text-lg font-semibold">Transfers</h4>
 
-          <Button variant="outline" size="sm">
-            <PlusCircle className="mr-1" />
-            New Transfer
-          </Button>
+          <SidePanel
+            buttonTrigger={
+              <Button variant="outline" size="sm">
+                <PlusCircle className="mr-1" />
+                New transfer
+              </Button>
+            }
+            saveButton={
+              <Button variant="outline" size="sm" form="create-transfer-form">
+                <SavePlus className="mr-1" />
+                Save transfer
+              </Button>
+            }
+            content={<CreateTransferForm />}
+            title="Create Transfer"
+            description="Fill in the details for the new transfer."
+          />
         </div>
 
         <TransferFilter />
