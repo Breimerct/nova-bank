27f3cb692afc111c328c26990904761c7f0347f9
breimerct <breimerct@gmail.com>
2026-06-23 18:07:06 -0500

    add SidePanel component

---
+++ src/components/side-panel/SidePanel.tsx
@@ -0,0 +1,52 @@
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet.tsx"
+import Button from "@/components/ui/button/button.tsx"
+import type { ReactNode } from "react"
+
+interface CreateTransferContentProps {
+  buttonTrigger: ReactNode
+  saveButton: ReactNode
+  content: ReactNode
+  title: string
+  description?: string
+}
+
+function SidePanel({
+  buttonTrigger,
+  saveButton,
+  content,
+  title,
+  description,
+}: CreateTransferContentProps) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>{buttonTrigger}</SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>{title}</SheetTitle>
+          {!!description && <SheetDescription>{description}</SheetDescription>}
+        </SheetHeader>
+
+        <div className="flex-1 px-4">{content}</div>
+
+        <SheetFooter>
+          {saveButton}
+
+          <SheetClose asChild>
+            <Button variant="outline">Close</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+export default SidePanel
