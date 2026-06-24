cffafbc212682b7e7cf1b93868c7e0c722e8609f
breimerct <breimerct@gmail.com>
2026-06-24 11:29:10 -0500

    add httpClient for API requests with authorization interceptor

---
+++ src/lib/http-client.ts
@@ -0,0 +1,31 @@
+import axios, {
+  type AxiosError,
+  type AxiosRequestConfig,
+  type InternalAxiosRequestConfig,
+} from "axios"
+
+const baseConfig: AxiosRequestConfig = {
+  baseURL: "/default",
+  headers: {
+    "Content-Type": "application/json",
+  },
+}
+
+export const httpClient = axios.create({ ...baseConfig, adapter: "fetch" })
+
+httpClient.interceptors.request.use(
+  async (config: InternalAxiosRequestConfig) => {
+    try {
+      const token = localStorage.token
+
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`
+      }
+    } catch (error) {
+      console.error("http-client interceptor failed to load auth store:", error)
+    }
+
+    return config
+  },
+  (error: AxiosError) => Promise.reject(error)
+)
