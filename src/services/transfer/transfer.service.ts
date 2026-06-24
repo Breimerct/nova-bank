e208ff541729fe6109591764682e342cfbbcc422
breimerct <breimerct@gmail.com>
2026-06-24 11:32:06 -0500

    add TransferService singleton for managing transfer requests

---
+++ src/services/transfer/transfer.service.ts
@@ -0,0 +1,22 @@
+import { BaseService } from "@/services/common/base.service.ts"
+import type { TransferResponseType } from "@/types/transfer.type.ts"
+
+export class TransferService extends BaseService {
+  private static instance: TransferService
+
+  private constructor() {
+    super()
+  }
+
+  static getInstance(): TransferService {
+    if (!TransferService.instance) {
+      TransferService.instance = new TransferService()
+    }
+
+    return TransferService.instance
+  }
+
+  async fetchTransfers() {
+    return await this.get<TransferResponseType>("/transferList")
+  }
+}
