e3e8dfdd6a462b5aad20ede9e25e379005bcd265
breimerct <breimerct@gmail.com>
2026-06-24 11:26:15 -0500

    add utility functions for error handling and initials extraction

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import type { TransferType } from "@/types/transfer.type.ts"
+import { isAxiosError } from "axios"
+import type { ClientRequest, IncomingMessage } from "http"
+import type { HttpProxy } from "vite"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -21,12 +24,42 @@ export function groupTransfersByDate(
   }, {})
 }
 
-export function formatCurrency(
-  value: number,
-  locale = "es-CO"
-): string {
+export function formatCurrency(value: number, locale = "es-CO"): string {
   return new Intl.NumberFormat(locale, {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(value)
+}
+
+export function extractMessageError(error: unknown): string {
+  if (typeof error === "string") {
+    return error
+  }
+
+  if (isAxiosError(error)) {
+    return error.response?.data?.message || error.message
+  }
+
+  if (error instanceof Error) {
+    return error.message
+  }
+
+  return "Unknown error"
+}
+
+export function extractInitials(fullName: string) {
+  if (!fullName) return "N/A"
+
+  const [name, lastname] = fullName.split(" ")
+  return `${name[0]}${lastname[0]}`.toUpperCase()
+}
+
+export function configureAuthHeader(proxy: HttpProxy.ProxyServer) {
+  proxy.on("proxyReq", (proxyReq: ClientRequest, req: IncomingMessage) => {
+    const authHeader = req.headers["authorization"]
+
+    if (authHeader) {
+      proxyReq.setHeader("Authorization", authHeader)
+    }
+  })
 }
